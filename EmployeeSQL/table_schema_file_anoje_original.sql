e40883b4da592f68e2213b75f07a09b7c8290d6f
AnojeJanathanan <131392884+AnojeJanathanan@users.noreply.github.com>
2024-03-11 09:56:51 -0700

    Add files via upload

---
+++ EmployeeSQL/table_schema_file_anoje_original.sql
@@ -0,0 +1,151 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.2
+-- Dumped by pg_dump version 16.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: departments; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.departments (
+    dept_no character(10) NOT NULL,
+    dept_name character varying(30) NOT NULL
+);
+
+
+ALTER TABLE public.departments OWNER TO postgres;
+
+--
+-- Name: dept_emp; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.dept_emp (
+    emp_no integer NOT NULL,
+    dept_no character(10) NOT NULL
+);
+
+
+ALTER TABLE public.dept_emp OWNER TO postgres;
+
+--
+-- Name: dept_manager; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.dept_manager (
+    dept_no character(10) NOT NULL,
+    emp_no integer NOT NULL
+);
+
+
+ALTER TABLE public.dept_manager OWNER TO postgres;
+
+--
+-- Name: employees; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.employees (
+    emp_no integer NOT NULL,
+    emp_title_id character varying(10),
+    birth_date date,
+    first_name character varying(30),
+    last_name character varying(30),
+    sex character(1),
+    hire_date date
+);
+
+
+ALTER TABLE public.employees OWNER TO postgres;
+
+--
+-- Name: salaries; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.salaries (
+    emp_no integer NOT NULL,
+    salary integer
+);
+
+
+ALTER TABLE public.salaries OWNER TO postgres;
+
+--
+-- Name: title; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.title (
+    title_id character varying(10) NOT NULL,
+    title character varying(30) NOT NULL
+);
+
+
+ALTER TABLE public.title OWNER TO postgres;
+
+--
+-- Name: departments departments_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.departments
+    ADD CONSTRAINT departments_pkey PRIMARY KEY (dept_no);
+
+
+--
+-- Name: dept_emp dept_emp_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.dept_emp
+    ADD CONSTRAINT dept_emp_pkey PRIMARY KEY (emp_no, dept_no);
+
+
+--
+-- Name: dept_manager dept_manager_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.dept_manager
+    ADD CONSTRAINT dept_manager_pkey PRIMARY KEY (dept_no, emp_no);
+
+
+--
+-- Name: employees employees_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.employees
+    ADD CONSTRAINT employees_pkey PRIMARY KEY (emp_no);
+
+
+--
+-- Name: salaries salaries_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.salaries
+    ADD CONSTRAINT salaries_pkey PRIMARY KEY (emp_no);
+
+
+--
+-- Name: title title_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.title
+    ADD CONSTRAINT title_pkey PRIMARY KEY (title_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
